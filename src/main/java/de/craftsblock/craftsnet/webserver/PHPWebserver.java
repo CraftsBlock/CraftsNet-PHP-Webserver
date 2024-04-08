fa1b8dcf892607fbe8135e560617aecd6f096f8c
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2024-04-08 10:33:21 +0200

    Initial Commit

---
+++ src/main/java/de/craftsblock/craftsnet/webserver/PHPWebserver.java
@@ -0,0 +1,17 @@
+package de.craftsblock.craftsnet.webserver;
+
+import de.craftsblock.craftsnet.addon.Addon;
+
+public class PHPWebserver extends Addon {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
